56d44c1a0bf2c57451f5847e7328fd58b58d2d92
Nikhilbest <ng929439@gmail.com>
2024-04-02 23:16:27 +0530

    added Dockerfile, styling

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
 def predict_api():
     data = request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
+    print(np.array(list(data.values())). reshape(1,-1))
     new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
